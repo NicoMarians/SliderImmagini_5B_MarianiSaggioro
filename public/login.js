62d8c48edd2ce0110aae895bdc709b4e24e286c6
FEDERICO <federico.saggiro@icloud.com>
2025-02-16 18:16:33 +0100

    navigator, login, html

--- public/login.js
+++ public/login.js
@@ -19,42 +19,6 @@ if (isLogged) {
   deleteButton.classList.remove("hidden");
 }
 
-const register = function (username, password) {
-  return fetch("conf.json")
-    .then((response) => response.json())
-    .then((confData) => {
-      return fetch("https://ws.cipiaceinfo.it/credential/register", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          key: confData.cacheToken,
-        },
-        body: JSON.stringify({ username, password }),
-      })
-        .then((response) => {
-          console.log("Stato HTTP:", response.status);
-          return response.json();
-        })
-        .then((result) => {
-          console.log("Corpo della risposta:", result);
-          if (result.result === "Ok") {
-            alert("Registrazione completata con successo!");
-            openModalButton.classList.remove("hidden");
-            modifyButton.classList.remove("hidden");
-            deleteButton.classList.remove("hidden");
-            sessionStorage.setItem("Logged", "true");
-          } else {
-            console.error("Errore durante la registrazione:", result);
-            alert("Registrazione fallita.");
-          }
-        })
-        .catch((error) => {
-          console.error("Errore registrazione:", error);
-          alert("Registrazione fallita.");
-        });
-    });
-};
-
 const login = function (username, password) {
   return fetch("conf.json")
     .then((response) => response.json())
